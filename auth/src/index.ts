916bf2156d3a20cda0eb448fcdb58ca1624e99cb
ThomasDos <dosanjos.thomas@gmail.com>
2022-06-27 15:51:13 +0200

    feat: 🎸 implement mongoose, user model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express, { json } from 'express'
 import { NotFoundError } from './errors/not-found-error'
 import { errorHandler } from './middlewares/error-handler'
+import mongoose from 'mongoose'
 
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
@@ -21,6 +22,18 @@ app.all('*', () => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log('Auth is listening on port 3000 !!')
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+
+    console.log('Connected to mongoDb')
+  } catch (error) {
+    console.log('error', error)
+  }
+
+  app.listen(3000, () => {
+    console.log('Auth is listening on port 3000 !!')
+  })
+}
+
+start()
